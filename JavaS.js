0b802415a2f35b0955eda3e572515f443b485e4d
ubcodigo-hue <ubcodigo@gmail.com>
2026-06-02 10:59:31 -0300

    cc

--- JavaS.js
+++ JavaS.js
@@ -1,10 +1,31 @@
-const itemsConSubmenu = document.querySelectorAll(".menu-principal li:has(ul)");
+const items = document.querySelectorAll(".menu-principal > ul > li");
 
-itemsConSubmenu.forEach((item) => {
-    const linkPrincipal = item.querySelector("a");
+items.forEach(item => {
 
-    linkPrincipal.addEventListener("click", (evento) => {
-        evento.preventDefault();
-        item.classList.toggle("abierto");
-    });
+    const submenu = item.querySelector("ul");
+
+    if (submenu) {
+
+        const enlace = item.querySelector("a");
+
+        enlace.addEventListener("click", function(e) {
+
+            if (!item.classList.contains("abierto")) {
+
+                e.preventDefault();
+
+                document.querySelectorAll(".menu-principal li.abierto")
+                    .forEach(li => {
+                        if (li !== item) {
+                            li.classList.remove("abierto");
+                        }
+                    });
+
+                item.classList.add("abierto");
+            }
+
+        });
+
+    }
+
 });
